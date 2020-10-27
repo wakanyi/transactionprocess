9268836617314dedee760cbd20306a23eb27f0d0
Mary Wakanyi <wakanyimary@gmail.com>
2020-10-27 09:31:36 +0000

    minor fixes

--- database/migrations/2020_10_27_092554_create_notifications_table.php
+++ database/migrations/2020_10_27_092554_create_notifications_table.php
@@ -16,14 +16,15 @@ class CreateNotificationsTable extends Migration
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
 	    $table->string('notification');
-	    $table->string('userID');
+	    $table->unsignedBigInteger('userID');
             $table->boolean('is_read')->default(0);
             $table->timestamps();
 
-	    $table->foreign('userID')
-		  ->references('userID')
+		$table->foreign('userID')
+		  ->references('id')
 		  ->on('users')
                   ->onDelete('cascade');
+        	
         });
     }
 
